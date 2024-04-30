46a7a4752f4a535a3096117e69184cb9419fea06
shamim-emon <emon.dev.bd@gmail.com>
2024-05-01 01:30:27 +0600

    Dependencies updated

--- app/src/androidTest/java/bd/emon/notes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/bd/emon/notes/ExampleInstrumentedTest.kt
@@ -12,7 +12,7 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest{
     @Test
     fun useAppContext() {
         // Context of the app under test.
